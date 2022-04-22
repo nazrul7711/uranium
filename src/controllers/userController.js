ada8b5e774d21c50ad6f3cff876c04bc0e01f96f
Nazrul7711 <nazrul7711@gmail.com>
2022-04-22 11:07:07 +0530

    authorisation and authentication with JWT

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  console.log(req.newAtribute);
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
+  let email = req.body.emailId;
   let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
+  let user = await userModel.findOne({ emailId: email, password: password });
   if (!user)
     return res.send({
       status: false,
@@ -31,33 +31,17 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FUnctionUp",
+      batch: "uranium",
+      organisation: "FunctionUp",
     },
-    "functionup-thorium"
+    "functionup-uranium"
   );
   res.setHeader("x-auth-token", token);
   res.send({ status: true, data: token });
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
   
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-thorium");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
@@ -84,7 +68,22 @@ const updateUser = async function (req, res) {
   res.send({ status: updatedUser, data: updatedUser });
 };
 
+const deleteUser = async function(req,res){
+  let userId = req.params.userId
+  let user = await userModel.findById(userId)
+  if(!user){
+    return res.send("there is no such user")
+  }
+  let updated = await userModel.findOneAndUpdate({_id:userId},{$set:{isDeleted:true}},{returnNewDocument:true})
+  res.send(updated)
+  
+
+
+
+}
+
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const req = require('express/lib/request');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const middle = require("../../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,8 +13,10 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",middle.auth, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",middle.auth, userController.updateUser)
+
+router.delete("/users/:userId",middle.auth, userController.deleteUser)
 
 module.exports = router;
