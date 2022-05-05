977d67b3781fb52d6af102f20779ade9c76c6ecb
aayushkr99 <ak858098@gmail.com>
2022-05-05 21:57:45 +0530

    almostDone

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose")
+const { isValidObjectId } = require("mongoose")
+
 const collegeModel = require("../models/collegeModel")
 const internModel = require("../models/internModel")
 
@@ -7,9 +10,9 @@ const isValid = function (value) {
     return true
 }
 
-// const isValidObjectid = function(ObjectId){
-//     return mongoose.Types.ObjectId.isValid(ObjectId)
-//   }
+const isValidObjectid = function(ObjectId){
+    return mongoose.Types.ObjectId.isValid(ObjectId)
+  }
 
 const createIntern = async function (req, res) {
     try {
@@ -18,41 +21,62 @@ const createIntern = async function (req, res) {
         if (Object.keys(data).length = 0) {
             res.status(400).send({ status: false, data: "Bad Request" })      // (400) = {the server cannot or will not process the request due to something that is perceived to be a client error}
         }
-        else {
-            const { name, email, mobile, collegeId} = data;
+        else 
+        {
+            const { name, email, mobile,collegeName } = data;
+
+            if (!isValid(name)) {
+                return res.status(400).send({
+                    status: false,
+                    msg: "name is required"
+                })
+            }
+
+            if (!isValid(email)) {
+                return res.status(400).send({
+                    status: false,
+                    msg: "email is required"
+                })
+            }
+
+            if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email)) {
+                return res.status(400).send({ status: false, msg: "Enter a valid email address" });
+            }
+
+            let uniqueEmail = await internModel.findOne({ email: data.email });// check email value present in collection or not
+            if(uniqueEmail) return res.status(400).send({ status: false, msg: "Email already exist" });
+
+            if (!isValid(mobile)) {
+                return res.status(400).send({
+                    status: false,
+                    msg: "mobile is required"
+                })
+            }
+
+            if (!/^[0-9]{10}$/.test(mobile)) {
+                return res.status(400).send({ status: false, msg: "Enter a valid mobile number" })
+            }
+            let uniqueMobile=await internModel.findOne({mobile:data.mobile});// check for the mobile no already exist or not 
+            if(uniqueMobile) return res.status(400).send({status:false, msg:"Mobile Number already exist"})
+
+            if(!isValid(collegeName)){
+                return res.status(400).send({status : false, msg : "Invalid College in request body"})
+            }
+            const findCollege = await collegeModel.findOne({name :data.collegeName, isDeleted : false})
+            if (!findCollege) {
+                return res.status(400).send({ msg: "College not found" })
+            }   else
+            {
+                
+                data.collegeId = findCollege._id
+            
+                let savedData = await internModel.create(data)
+                res.status(201).send({ status: true,msg: "Registered successfully for internship ", data: savedData })
+            }
+     
+        }
         
-        if (!isValid(name)) {
-            return res.status(400).send({
-                status: false,
-                msg: "name is required"
-            })
-
-        }
-        if (!isValid(email)) {
-            return res.status(400).send({
-                status: false,
-                msg: "fullName is required"
-            })
-        }
-        if(!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email)){
-            return res.status(400).send({status: false, msg : "Enter a valid email address"});
-        }
-
-        if (!isValid(mobile)) {
-            return res.status(400).send({
-                status: false,
-                msg: "logoLink is required"
-            })
-        }
-
-        if(!/^[0-9]{10}$/.test(mobile)){
-            return res.status(400).send({status : false, msg : "Enter a valid mobile number"})
-        }
-        
-        let savedData = await internModel.create(data)
-        res.status(201).send({ status: true, data: savedData })
     }
-}
 
     catch (err) {
         console.log(err)
